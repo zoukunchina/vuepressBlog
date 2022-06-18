99468de9fedeed4bc7dad7b5415c35bcbd5e9a2d
kzou <kzou@amarsoft.com>
2022-06-18 17:40:34 +0800

    修改推送分支main->master

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果你想要部署到 https://<USERNAME>.github.io  
-git push -f git@github.com:zoukunchina/zoukunchina.github.io.git main
+git push -f git@github.com:zoukunchina/zoukunchina.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>  REPO=github上的项目
 # git push -f git@github.com:<USERNAME>/vuepress.git master:gh-pages
